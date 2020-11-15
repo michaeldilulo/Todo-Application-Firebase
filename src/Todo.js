52734d2ac5163da2e2a65041570f5558843966a6
michael dilulo <snoleopard01001101@gmail.com>
2020-11-15 09:54:44 -0800

    Modal pops up for edit

--- src/Todo.js
+++ src/Todo.js
@@ -1,21 +1,33 @@
 // rfce = React Functional Component w/ export
 
-import { Button, List, ListItem, ListItemText } from '@material-ui/core'
+import { Button, List, ListItem, ListItemText, Modal } from '@material-ui/core'
 import './Todo.css'
-import React from 'react'
+import React, { useState } from 'react'
 import db from './firebase'
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever'
 
 function Todo(props) {
+    // keep track of what open is 
+    const [open, setOpen] = useState(false);
+
     return (
-        <List className="todo__list">
-            <ListItem>
-                <ListItemText primary="Todo Item" secondary={props.todo.todo}></ListItemText>
-            </ListItem>
-            <DeleteForeverIcon onClick={event =>
-                db.collection('todos').doc(props.todo.id).delete()
-            }></DeleteForeverIcon>
-        </List>
+        <>
+            <Modal open={open} onClose={e => setOpen(false)}>
+                <div>
+                    <h1>open</h1>
+                    <button onClick={e => setOpen(false)}></button>
+                </div>
+            </Modal>
+            <List className="todo__list">
+                <ListItem>
+                    <ListItemText primary="Todo Item" secondary={props.todo.todo}></ListItemText>
+                </ListItem>
+                <button onClick={e => setOpen(true)}>Edit</button>
+                <DeleteForeverIcon onClick={event =>
+                    db.collection('todos').doc(props.todo.id).delete()
+                }></DeleteForeverIcon>
+            </List>
+        </>
     )
 }
 
