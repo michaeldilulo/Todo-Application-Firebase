99af37a618a4aaef5560dfa1a9e6c629eb49314f
michael dilulo <snoleopard01001101@gmail.com>
2020-11-15 07:03:45 -0800

    Notes - useState Hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React, { useState } from 'react';
 import './App.css';
 
+// PROPS - Properties
+// STATE - Short term memory (will clear when page is refreshed)
+// Can run dynamic JS with JSX (Javascript, X is hybrid with HTML)
+// const [todos, setTodos] - const set up variable, list of todos (array), when we use useState hook, we need something to call that will allow us to change the variable (setTodos)
+
 function App() {
+  const [todos, setTodos] = useState([])
+
+
   return (
     <div className="App">
-      <h1>Lets Create a Todo Application with Firebase</h1>
+      <h1>Todo Application with Firebase</h1>
+      <input type="text" />
+      <button>Add Todo</button>
+
+      <ul>
+        <li>Take Maya for a Walk</li>
+        <li>Take the trash out</li>
+      </ul>
     </div>
   );
 }
